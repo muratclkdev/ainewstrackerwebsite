ca1162e70d899b77930493f6163f80f14fb048c3
Murat Celik <murat.celik1290@gmail.com>
2025-01-16 23:55:33 +0300

    mobile update

--- app/page.tsx
+++ app/page.tsx
@@ -237,7 +237,7 @@ export default function Home() {
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
         process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
         form,
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+        'RuTlqyGrtfjYDeqgt'
       );
       setIsSubmitted(true);
     } catch (error) {
